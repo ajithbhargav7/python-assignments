b276b2fb399a820f2e04b70598d919809e1b01ca
ajithbhargav7 <ajithbhargav7@gmail.com>
2025-08-12 17:57:37 +0530

    Add files via upload

---
+++ a1t1.py
@@ -0,0 +1,10 @@
+num1 = int(input("enter a nuumber:"))
+num2 = int(input("enter another nuumber:"))
+sum= num1+num2
+print("\nAddition:",sum)
+sub=num1 - num2
+print("Subtraction:",sub)
+mul=num1 * num2
+print("Multiplication",mul)
+div=num1/num2
+print("Division:",div)       
---
+++ a1t2.py
@@ -0,0 +1,3 @@
+fn=input("enter your first name:")
+ln=input("enter your last name:")
+print("\n Hello,\t"+fn+"\t"+ln+"\twelcome to the python program.")
